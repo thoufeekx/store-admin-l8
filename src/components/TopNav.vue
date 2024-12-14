46b6f8b0855f1c8bce0fb51ec1951505f5fed9a3
thoufeekx <thoufeekbaber1@gmail.com>
2024-12-14 01:19:13 -0500

    updated color

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -40,7 +40,7 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #570303;
+  background-color: #7a7805;
   color: #fff;
   padding-top: 0.5rem;
   padding-left: 1rem;
